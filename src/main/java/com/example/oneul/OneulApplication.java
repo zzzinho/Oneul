2de7cc13b13acdd484641260eaedd35869e96537
jinho jeong <hahoh0013@gmail.com>
2022-04-15 17:21:04 +0900

    init

---
+++ src/main/java/com/example/oneul/OneulApplication.java
@@ -0,0 +1,13 @@
+package com.example.oneul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/oneul/OneulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oneul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
