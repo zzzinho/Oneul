3088a7ea9fcf5f42d725f4a0d45a7db96c0c1b05
jinho jeong <hahoh0013@gmail.com>
2022-06-04 13:16:56 +0900

    prod configuration

--- src/main/java/com/example/oneul/global/config/MongoConfig.java
+++ src/main/java/com/example/oneul/global/config/MongoConfig.java
@@ -1,23 +1,31 @@
 package com.example.oneul.global.config;
 
-import com.mongodb.ConnectionString;
-import com.mongodb.MongoClientSettings;
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoClients;
-
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+
 @Configuration
 @EnableJpaRepositories(basePackages = {"com.example.oneul.domain.post.dao.command", "com.example.oneul.domain.user.dao"})
 @EnableMongoRepositories(basePackages = "com.example.oneul.domain.post.dao.query")
 public class MongoConfig {
+    @Value("${spring.data.mongodb.host}")
+    private String host;
+    @Value("${spring.data.mongodb.port}")
+    private Integer port;
+    @Value("${spring.data.mongodb.database}")
+    private String database;
+
     @Bean
     public MongoClient mongo() {
-        ConnectionString connectionString = new ConnectionString("mongodb://localhost:27017/oneul");
+        ConnectionString connectionString = new ConnectionString("mongodb://" + host + ":" + port + "/" + database);
         MongoClientSettings mongoClientSettings = MongoClientSettings.builder()
           .applyConnectionString(connectionString)
           .build();
